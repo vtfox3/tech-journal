95a49063759b2ab5978c91d4958eac88a5f8d561
james.fox <james.fox@mymail.champlain.edu>
2020-02-17 11:30:04 -0500

    added executable file

---
+++ sys265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author james.fox
+#creates a new ssh user using $l parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
